a5da9ae4e0244045ecc7b0c1df3774d1b88a01bb
Thomas Poschen <mauzundwauz@users.noreply.github.com>
2025-03-21 18:54:18 +0100

    Update supabase.ts
    Anpassung an login

--- stille-sterne/lib/supabase.ts
+++ stille-sterne/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    redirectTo: 'https://stille-sterne-frontend.vercel.app/dashboard',
+  },
+})
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export default supabase
