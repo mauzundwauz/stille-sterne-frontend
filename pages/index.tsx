4a8552457302f46cfe1089a2e3d52d3d6ea9f5f3
Thomas Poschen <mauzundwauz@users.noreply.github.com>
2025-03-21 19:14:24 +0100

    Vollständiges Frontend Starter Paket

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Willkommen bei Stille Sterne ✨</h1>
+      <p>Dies ist deine Starterseite – verbunden mit Supabase.</p>
+    </main>
+  )
+}
